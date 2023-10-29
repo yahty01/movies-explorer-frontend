76087fe929793d0ccb2e7be58b3bb07f6e57b63e
YAHTY <119876639+yahty01@users.noreply.github.com>
2023-10-29 16:55:57 +0300

    fix

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
-import Header from '../Header/Header';
-import './Profile.scss';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import Header from "../Header/Header";
+import "./Profile.scss";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
-  const [name, setName] = useState('Егор');
-  const [email, setEmail] = useState('mail@yandex.ru');
+  const [name, setName] = useState("Егор");
+  const [email, setEmail] = useState("mail@yandex.ru");
   const [isEditing, setIsEditing] = useState(false);
   const navigate = useNavigate();
 
-  console.log('рендеринг');
+  console.log("рендеринг");
 
   const handleEditClick = () => {
     setIsEditing(true);
@@ -23,50 +23,89 @@ function Profile() {
   };
 
   const handleSignoutClick = () => {
-    navigate('/signout');
+    navigate("/signout");
   };
-  
+
   const handleSaveClick = () => {
-    console.log('click');
+    console.log("click");
     setIsEditing(false);
   };
   return (
     <>
-      <Header mode='dark' />
-      <section className='profile'>
-        <div className='profile__container'>
-          <h1 className='profile__heading'>Привет, Егор!</h1>
-          <div className='profile__name-row'>
-            <label htmlFor='name' className='profile__name-label'>
-              Имя
-            </label>
-            {isEditing ? <input type='text' name='name' minLength='2' value={name} onChange={handleNameChange} className='profile__field-name' /> : <span className='profile__name-value'>{name}</span>}
+      <Header mode="dark" />
+      <main>
+        <section className="profile">
+          <div className="profile__container">
+            <h1 className="profile__heading">Привет, Егор!</h1>
+            <div className="profile__name-row">
+              <label htmlFor="name" className="profile__name-label">
+                Имя
+              </label>
+              {isEditing ? (
+                <input
+                  type="text"
+                  name="name"
+                  minLength="2"
+                  value={name}
+                  onChange={handleNameChange}
+                  className="profile__field-name"
+                />
+              ) : (
+                <span className="profile__name-value">{name}</span>
+              )}
+            </div>
+            <div className="profile__email-row">
+              <label htmlFor="email" className="profile__email-label">
+                E-mail
+              </label>
+              {isEditing ? (
+                <input
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  className="profile__field-email"
+                />
+              ) : (
+                <span className="email-value">{email}</span>
+              )}
+            </div>
+            {isEditing ? (
+              <>
+                <span className="profile__error">
+                  При обновлении профиля произошла ошибка.
+                </span>
+                <button
+                  type="button"
+                  className="profile__button profile__button_disabled"
+                  onClick={handleSaveClick}
+                  aria-label="Сохранить данные"
+                >
+                  Сохранить
+                </button>
+              </>
+            ) : (
+              <>
+                <span
+                  className="button profile__link-edit"
+                  onClick={handleEditClick}
+                  aria-label="Редактировать профиль"
+                >
+                  Редактировать
+                </span>
+                <button
+                  type="button"
+                  className="button profile__link-exit"
+                  onClick={handleSignoutClick}
+                  aria-label="Выйти из аккаунта"
+                >
+                  Выйти из аккаунта
+                </button>
+              </>
+            )}
           </div>
-          <div className='profile__email-row'>
-            <label htmlFor='email' className='profile__email-label'>
-              E-mail
-            </label>
-            {isEditing ? <input type='email' name='email' value={email} onChange={handleEmailChange} className='profile__field-email' /> : <span className='email-value'>{email}</span>}
-          </div>
-          {isEditing ? (
-             <>
-             <span className='profile__error'>При обновлении профиля произошла ошибка.</span>
-             <button type='button' className='profile__button profile__button_disabled' onClick={handleSaveClick} aria-label='Сохранить данные'>
-               Сохранить
-             </button>
-           </>
-         ) : (
-           <>
-             <span className='button profile__link-edit' onClick={handleEditClick} aria-label='Редактировать профиль'>
-               Редактировать
-             </span>
-             <button type='button' className='button profile__link-exit' onClick={handleSignoutClick} aria-label='Выйти из аккаунта'>
-               Выйти из аккаунта
-             </button>
-           </>
-          )}
-        </div>
-      </section>
+        </section>
+      </main>
     </>
   );
 }
